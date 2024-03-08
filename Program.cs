3f0b290d30999ba584ecdc9f3940b57f8a162eb4
Jaqueline <csi23-jromres@colegioaltair.net>
2024-03-08 12:16:07 +0100

    no message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ejercicio7Repaso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca una frase");
+            string frase = Console.ReadLine();
+            Console.WriteLine(frase.Length);
+        }
+    }
+}
